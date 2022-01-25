0c21bbf10746c87c2ac55494fd3f78e8548b91a4
dongkam <84716824+dongkam5@users.noreply.github.com>
2022-01-26 01:30:20 +0900

    랜덤한 시간동안 output 출력하기

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const startPauseBtn=document.querySelector(".btn #startPause");
 const restartBtn=document.querySelector(".btn #restart");
 const timer = document.querySelector('.timer #timerTime');
+const box= document.querySelector('.playground');
 
 let startTime=0;
 let spendTime=0;
 let savedTime=0;
-let interval;
+let timeInterval;
+let appearInterval;
+let element= new Array();
+let interTime=1000;
 startPauseBtn.addEventListener('click',(event)=>{
     if(! startPauseBtn.classList.contains('check')){
         startTime= new Date();
         startPauseBtn.classList.toggle('check');
         event.target.innerHTML='Pause';
-        interval=setInterval(()=>{
+        timeInterval=setInterval(()=>{
             date= new Date();
             diff=date-startTime+savedTime;
             ms=String(parseInt((diff%1000)/10)).padStart(2,'0');
@@ -19,18 +23,23 @@ startPauseBtn.addEventListener('click',(event)=>{
             min=String(parseInt(sec/60)).padStart(2,'0');
             timer.innerHTML=`${min}:${sec}:${ms}`
         },1)
+        appearInterval = setInterval(()=>{
+            interTime=parseInt((Math.random()*5000));
+            alert('hi');
+        },interTime);
     }
     else{
         startPauseBtn.classList.toggle('check');
         event.target.innerHTML='Start';
         savedTime=diff;
-        clearInterval(interval);
+        clearInterval(timeInterval);
+        clearInterval(appearInterval);
     }
     
 });
-
 restartBtn.addEventListener('click',()=>{
-    clearInterval(interval);
+    clearInterval(appearInterval);
+    clearInterval(timeInterval);
     timer.innerHTML='00:00:00';
     savedTime=0;
     if(startPauseBtn.classList.contains('check')){
@@ -38,3 +47,4 @@ restartBtn.addEventListener('click',()=>{
         startPauseBtn.innerHTML='Start';
     }
 })
+
